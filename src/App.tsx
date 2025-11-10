cc454d288cce5a89721c9e08726adf06324f49ee
ADaneshS1 <daneshsudiono@gmail.com>
2025-11-10 19:52:55 +0700

    delete profile img

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Profile from "./profile";
 import { Tasks } from "./tasks";
 
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <Profile />
+      <h1 className="font-bold">Hello World</h1>
       <Tasks />
     </>
   );
