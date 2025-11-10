7d85c39622cd3c471ccfc144819df45aeabe35a6
ADaneshS1 <daneshsudiono@gmail.com>
2025-11-10 16:54:16 +0700

    feat: profile curly braces

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Profile from "./profile";
+
 export default function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Profile />
+      <Profile />
+      <Profile />
     </>
   );
 }
---
+++ src/profile.tsx
@@ -0,0 +1,5 @@
+export default function Profile() {
+  const image = "https://i.imgur.com/MK3eW3Am.jpg";
+  const alt = "Johnson";
+  return <img src={image} alt={alt} />;
+}
