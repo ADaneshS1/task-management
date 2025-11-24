a7b75c75f29b68ace7fd5bca9b99954bee181e42
ADaneshS1 <daneshsudiono@gmail.com>
2025-11-24 18:30:14 +0700

    feat: navbar layout

---
+++ src/layouts/layout.tsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from "react-router";
+
+export function Layout() {
+  return (
+    <section>
+      <nav className="flex items-center justify-between gap-4">
+        <h1 className="my-4 text-2xl font-bold text-sky-700">
+          <Link to="/">Task Management</Link>
+        </h1>
+
+        <ul className="inline-flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </section>
+  );
+}
