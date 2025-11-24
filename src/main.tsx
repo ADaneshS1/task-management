2f3a7bef52986a79f0ff8e1c2d48d3201e5c3e05
ADaneshS1 <daneshsudiono@gmail.com>
2025-11-24 19:02:28 +0700

    feat: layouts to main

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import App from "@/app";
 import About from "@/routes/about";
 
 import { BrowserRouter, Routes, Route } from "react-router";
+import { Layout } from "./layouts/layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
