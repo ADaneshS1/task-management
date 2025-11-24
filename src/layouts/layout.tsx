c63ffaf0b9ccfcdc5a27e8f0f2a28d31bb563031
ADaneshS1 <daneshsudiono@gmail.com>
2025-11-24 19:38:11 +0700

    feat: change layout style

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -3,22 +3,30 @@ import { Link, Outlet } from "react-router";
 export function Layout() {
   return (
     <section>
-      <nav className="flex items-center justify-between gap-4">
-        <h1 className="my-4 text-2xl font-bold text-sky-700">
-          <Link to="/">Task Management</Link>
-        </h1>
+      <nav className="w-full bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto flex items-center justify-between px-6 lg:px-12 py-4">
+          <h1 className="text-2xl font-bold text-sky-700">
+            <Link to="/">Task Management</Link>
+          </h1>
 
-        <ul className="inline-flex gap-4">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
+          <ul className="inline-flex gap-6 text-gray-700 font-medium">
+            <li>
+              <Link to="/" className="hover:text-sky-600">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:text-sky-600">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
 
-      <Outlet />
+      <main className="max-w-7xl mx-auto px-6 lg:px-12">
+        <Outlet />
+      </main>
     </section>
   );
 }
